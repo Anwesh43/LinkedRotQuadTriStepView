bd4ddf765210d5a9a426a688389d77ce393bd945
Anwesh43 <anweshthecool0@gmail.com>
2018-11-28 02:23:28 +0530

    adding Int and Float extension methods

--- rotquadtristepview/src/main/java/com/anwesh/uiprojects/rotquadtristepview/RotQuadTriStepView.kt
+++ rotquadtristepview/src/main/java/com/anwesh/uiprojects/rotquadtristepview/RotQuadTriStepView.kt
@@ -20,3 +20,13 @@ val scDiv : Double = 0.51
 val color : Int = Color.parseColor("#0D47A1")
 val sizeFactor : Float = 2.5f
 val strokeFactor : Int = 120
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
