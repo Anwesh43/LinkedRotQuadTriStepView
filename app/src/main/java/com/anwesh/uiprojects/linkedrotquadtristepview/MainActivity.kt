fa63bd8d593611879f27d36fc09c56a674a98b4a
Anwesh43 <anweshthecool0@gmail.com>
2018-11-28 03:59:46 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrotquadtristepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotquadtristepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrotquadtristepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rotquadtristepview.RotQuadTriStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotQuadTriStepView.create(this)
     }
 }
