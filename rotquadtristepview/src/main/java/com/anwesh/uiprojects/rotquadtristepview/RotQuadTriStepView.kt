3c4cc84958b527b30e6b3d35e41fc83aa6ebdc07
Anwesh43 <anweshthecool0@gmail.com>
2018-11-28 03:33:51 +0530

    adding RQTSNode class

--- rotquadtristepview/src/main/java/com/anwesh/uiprojects/rotquadtristepview/RotQuadTriStepView.kt
+++ rotquadtristepview/src/main/java/com/anwesh/uiprojects/rotquadtristepview/RotQuadTriStepView.kt
@@ -124,4 +124,49 @@ class RotQuadTriStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RQTSNode(var i : Int, val state : State = State()) {
+
+        private var next : RQTSNode? = null
+
+        private var prev : RQTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RQTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRQTSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RQTSNode {
+            var curr : RQTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
